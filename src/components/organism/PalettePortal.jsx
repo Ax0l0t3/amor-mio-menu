13eee6be123963a201f0f01521ca2997cb70b03d
D4v3 <antenopolis@gmail.com>
2024-12-08 10:31:47 -0600

    Palette created

--- src/components/organism/PalettePortal.jsx
+++ src/components/organism/PalettePortal.jsx
@@ -1,7 +1,43 @@
-export const PalettePortal = () => {
-  return (
-    <div className="p-2 bg-white">
+import "../../styles/_palette-portal.css"
 
+export const PalettePortal = () => {
+  const returnColours = [
+    "#1f1612ff",
+    "#454a48ff",
+    "#828d51ff",
+    "#f7df44ff",
+    "#d5b639ff",
+    "#999999ff",
+    "#e6e6e6ff",
+    "#1c0310ff",
+  ];
+
+  const gradients = [
+    {channel: "red-gradient", value: 0},
+    {channel: "green-gradient", value: 239},
+    {channel: "blue-gradient", value: 132},
+    {channel: "alpha-gradient", value: 255}
+  ];
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-2 rounded-lg flex flex-col">
+        <div className="text-black">
+          {gradients.map((obj, id) => (
+            <div key={id} className="flex items-center">
+              <div className={obj.channel} />
+              <p className="p-gradient">{obj.value}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex">
+          <div className="bg-[#977f33ff] w-[4.5rem] h-[4.5rem] rounded-l-lg" />
+          {returnColours.map((color, id) => (
+            <div key={id} style={{ background: color }} className="w-[4.5rem] h-[4.5rem]" />
+          ))}
+          <div className="bg-[#808080ff] w-[4.5rem] h-[4.5rem] rounded-r-lg" />
+        </div>
+      </div>
     </div>
   )
 }
