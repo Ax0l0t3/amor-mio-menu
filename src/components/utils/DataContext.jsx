8429efb2127188b56f4d709bfe9363e348ed45e6
David Leon <d.leon@a1qa.com>
2025-03-05 14:41:16 -0600

    Save changes to PrintContext

--- src/components/utils/DataContext.jsx
+++ src/components/utils/DataContext.jsx
@@ -2,3 +2,4 @@ import { createContext } from "react";
 import MockData from "../../../mockData.json";
 
 export const DataContext = createContext(MockData);
+export const PrintContext = createContext([]);
