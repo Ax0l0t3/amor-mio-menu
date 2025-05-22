7f80b5181c28c0ad84c32886e72b14c36e745f06
David Leon <d.leon@a1qa.com>
2025-05-21 19:01:59 -0600

    Add-modal cancelation button

---
+++ src/stories/atom/TextBox.stories.jsx
@@ -0,0 +1,11 @@
+import { TextButton } from "../../components/atom/TextButton";
+
+export default {
+    component: TextButton,
+};
+
+export const Default = () => {
+    return (
+        <TextButton />
+    );
+};
