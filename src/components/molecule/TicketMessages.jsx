944f7424e96e64d9675025b6d9cedd1381d65c1a
David Leon <d.leon@a1qa.com>
2025-07-09 23:15:11 -0600

    Preprint ticket preview info management

--- src/components/molecule/TicketMessages.jsx
+++ src/components/molecule/TicketMessages.jsx
@@ -11,16 +11,19 @@ export const TicketMessages = ({ dishes }) => {
     setTicketMessages(messagesByLevels(dishes, propsToOrder));
   }, [dishes]);
 
-  return ticketMessages.map((message) => {
+  return ticketMessages.map((message, coreIndex) => {
     const messageSplitted = message.split("\t");
-    const lineBreaks = messageSplitted[1].split(",");
+    const ingsMessage = coreIndex === 0 ? messageSplitted[1] : messageSplitted[1].slice(4);
+    const ingsSplitted = ingsMessage.split(",");
     const marginValue = (messageSplitted[0] * 4) / 4;
     const pStyle = { marginLeft: `${marginValue}rem` };
+    const dishQtty = messageSplitted[1].at(0);
     return (
       <div style={pStyle} key={message}>
-        {lineBreaks.map((paragraph, index) => (
+        {coreIndex !== 0 && <span>({dishQtty})</span>}
+        {ingsSplitted.map((paragraph, index) => (
           <ul key={index}>
-            <li>{`${index === 0 ? "" : "- > "}${paragraph}`}</li>
+            <li>{`${coreIndex ===0 ? "" : "- > "}${paragraph}`}</li>
           </ul>
         ))}
       </div>
