6253a7f67d1f26de29aad0c77e493f006111096e
David Leon <d.leon@a1qa.com>
2025-07-11 18:57:41 -0600

    Linter fixes; Prettier changes

--- src/components/molecule/TicketMessages.jsx
+++ src/components/molecule/TicketMessages.jsx
@@ -13,7 +13,8 @@ export const TicketMessages = ({ dishes }) => {
 
   return ticketMessages.map((message, coreIndex) => {
     const messageSplitted = message.split("\t");
-    const ingsMessage = coreIndex === 0 ? messageSplitted[1] : messageSplitted[1].slice(4);
+    const ingsMessage =
+      coreIndex === 0 ? messageSplitted[1] : messageSplitted[1].slice(4);
     const ingsSplitted = ingsMessage.split(",");
     const marginValue = (messageSplitted[0] * 4) / 4;
     const pStyle = { marginLeft: `${marginValue}rem` };
@@ -23,7 +24,7 @@ export const TicketMessages = ({ dishes }) => {
         {coreIndex !== 0 && <span>({dishQtty})</span>}
         {ingsSplitted.map((paragraph, index) => (
           <ul key={index}>
-            <li>{`${coreIndex ===0 ? "" : "- > "}${paragraph}`}</li>
+            <li>{`${coreIndex === 0 ? "" : "- > "}${paragraph}`}</li>
           </ul>
         ))}
       </div>
