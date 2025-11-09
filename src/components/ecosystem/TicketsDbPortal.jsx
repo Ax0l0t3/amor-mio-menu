ba0dec604d34146f0cb09db62a3b6bbe4b9add03
David Leon <d.leon@a1qa.com>
2025-11-09 00:40:22 -0600

    Tickets open/close action development

--- src/components/ecosystem/TicketsDbPortal.jsx
+++ src/components/ecosystem/TicketsDbPortal.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 // Molecule
 import { TicketMessages } from "../molecule/TicketMessages";
@@ -13,9 +13,13 @@ import { formatDate } from "../utils/DateUtils";
 
 // Styles
 import "../../styles/ecosystem/_tickets-db-portal.css";
+import { PrinterIconSvg } from "../atom/PrinterIcon";
+import { SvgButton } from "../molecule/SvgButton";
+import { AddSVG } from "../atom/AddIcon";
 
 export const TicketsDbPortal = ({ closePortal = Function.prototype }) => {
   const { ticketsContext } = useContext(TicketsContext);
+  const [selectedTicket, setSelectedTicket] = useState(formatDate(new Date()));
 
   const menuButtons = [
     {
@@ -33,6 +37,11 @@ export const TicketsDbPortal = ({ closePortal = Function.prototype }) => {
       </div>
     );
   };
+
+  const handleSelectedTicket = (stringDate) => {
+    setSelectedTicket(stringDate);
+  };
+
   const returnTickets = () => {
     ticketsContext.sort((a, b) => {
       const dateA = a.NowDate.toUpperCase();
@@ -47,14 +56,52 @@ export const TicketsDbPortal = ({ closePortal = Function.prototype }) => {
       return 0;
     });
     const sortedTickets = ticketsContext.map((obj, i) => {
-      const objDate = new Date(obj.NowDate);
+      const objDate = formatDate(new Date(obj.NowDate));
       const objOrder = obj.PrintedObjects[0].Order;
-      return (
-        <div className="pre-show-card" key={i}>
-          <h6>{formatDate(objDate)}</h6>
-          {objOrder && <h6>Orden para: {objOrder}</h6>}
-          <TicketMessages dishes={obj.PrintedObjects} />
+      const isSelected = objDate === selectedTicket;
+      const seventhElement = i === 6;
+      const seventhCss = seventhElement && isSelected ? "seventh-element" : "";
+      const selectedCss = `pre-show-card${isSelected ? "-selected" : ""} ${seventhCss}`;
+      return isSelected ? (
+        <div className={selectedCss} key={i}>
+          <div className={`overflow-container`}>
+            <div>
+              <h6>{objDate}</h6>
+              {objOrder && <h6>Orden para: {objOrder}</h6>}
+              <TicketMessages dishes={obj.PrintedObjects} />
+            </div>
+            {isSelected && (
+              <div className="ticket-icons">
+                <SvgButton
+                  className="ticket-close"
+                  clickAction={() => setSelectedTicket(formatDate(new Date()))}
+                >
+                  <AddSVG svgHeight={16} svgWidth={16} />
+                </SvgButton>
+                <SvgButton>
+                  <PrinterIconSvg
+                    tailwindClass="m-0"
+                    svgHeight={24}
+                    svgWidth={24}
+                  />
+                </SvgButton>
+              </div>
+            )}
+          </div>
         </div>
+      ) : (
+        <button
+          className={selectedCss}
+          key={i}
+          onClick={() => handleSelectedTicket(objDate)}
+          type="button"
+        >
+          <div>
+            <h6>{objDate}</h6>
+            {objOrder && <h6>Orden para: {objOrder}</h6>}
+            <TicketMessages dishes={obj.PrintedObjects} />
+          </div>
+        </button>
       );
     });
     return <div className="tickets-div">{sortedTickets}</div>;
